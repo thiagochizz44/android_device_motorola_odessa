734d76c769ced9f6f2317b60097b1bb0d74c9f6f
Thiago <diasthiago@protonmail.com>
2025-01-21 07:46:57 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,6 @@ deviceDir=$(gettop)/device/motorola/odessa
 
 # apply patches
 ${deviceDir}/applyPatches.sh ${deviceDir}/patches
+
+# Clone BCR
+git clone https://github.com/Chaitanyakm/vendor_bcr.git -b main vendor/bcr
